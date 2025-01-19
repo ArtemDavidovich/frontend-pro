e1052e9e8c64210e2ce1483c0a493bc4cd1514c8
Artem Davidovich <chumazek@gmail.com>
2025-01-19 18:59:40 +0100

    homework2 profile card

---
+++ src/components/profileCard/ProfileCard.jsx
@@ -0,0 +1,26 @@
+import './profileCard.css'
+
+/* eslint-disable react/prop-types */
+function ProfileCard({character}) {
+
+    const {image, name, profession, hobby} = character;
+
+    return (
+        <div className="profileCardContainer">
+            <div className='profileCardBackground'>
+                <div className="profileCard">
+                    <img src={character.image} height={300} alt={character.name} />
+                    <h2>{character.name}</h2>
+                    <h3>Profession: </h3>
+                    <p>{character.profession}</p> 
+                    <h3>Hobby: </h3> 
+                    <p>{character.hobby}</p>                 
+                </div>
+            </div>
+        </div>
+    )
+}
+/* eslint-enable react/prop-types */
+
+export default ProfileCard;
+
